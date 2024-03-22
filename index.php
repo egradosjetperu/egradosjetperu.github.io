6bb60147c50e4d8c54424291c85c4de68aab260c
egradosjetperu <143428742+egradosjetperu@users.noreply.github.com>
2024-03-22 16:45:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,7 @@
     <title>Test Page</title>
 </head>
 <body>
-    <h1>Test Page</h1>
-    <?php
-    // PHP code to make GET request
-    $url = 'https://jetperuapis.pe/api/v2/custom/test/test';
-    $response = file_get_contents($url);
-    echo "<p>Response from API: $response</p>";
-    ?>
+    <h1>Test Page PHP</h1>
+
 </body>
 </html>
